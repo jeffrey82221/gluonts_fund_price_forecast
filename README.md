ce5d4110c3bf767818cdbe65b1076a7a1cd06bef
jeffrey82221 <jeffrey82221@gmail.com>
2022-01-23 12:09:09 +0800

    [add] load and process nav file

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ This project aims to predict the future price of mutual fund via GluonTS package
 
 # TODO: 
 
-- [ ] Load all fund price csv file into Gluonts time series object 
-- [ ] Process the time series so that the missing prices of holiday can be interpolate
-- [ ] Connect the time series to the training module of a Gluonts Model 
-- [ ] Plot the predicted trends (probalistically) from the trained model. 
+- [X] Price Prediction of Single Fund
+    - [X] Load fund price csv file into Gluonts time series object. 
+    - [X] Process the time series so that the missing prices of holiday can be interpolate.
+    - [X] Connect the time series to the Gluonts Model 
+    - [ ] Plot the predicted trends (probalistically) from the trained model. 
+- [ ] Adapt the Model to the Deep Trainable Models
+- [ ] Price Prediction of Multiple Funds 
+    - [ ] Load multiple funds and convert to multiple time series 
+    - [ ] Parallel loading and processing of multiple time series
+    - [ ] Consider multivariate time series Model 
+    - [ ] Plot the predicted trends 
--- fund_price_loader.py
+++ fund_price_loader.py
@@ -1,3 +1,68 @@
+import os
+from datetime import datetime
 import pandas as pd
+from gluonts.dataset.common import ListDataset
+from gluonts.model import prophet
 
+NAV_DIR = '../fund_price_crawler/nav'
 
+def load_raw_nav_table(file_path):
+    """
+    Load the NAV csv into pandas DataFrame 
+
+    Args: 
+        - file_path: the path of the nav csv file
+    Returns: 
+        - nav_table: the loaded nav pandas DataFrame.
+    """
+    nav_table = pd.read_csv(file_path,
+                            delimiter=',', header=None, index_col=False)
+    nav_table.columns = ['date', 'value']
+    nav_table.drop_duplicates(inplace=True)
+    return nav_table
+
+def fill_nav_dataframe(raw_nav_table):
+    """
+    
+    Fill in the nav values of holidays via linear interpolation. 
+
+    Args: 
+        - raw_nav_table: the raw nav dataframe loaded from the downloaded csv file
+    Returns:
+        - everyday_nav_table: a pandas DataFrame of nav values without missing values 
+            of holiday. 
+    """
+    raw_nav_table.date = raw_nav_table.date.map(lambda x: datetime.strptime(x, '%Y/%m/%d'))
+    idx = pd.date_range(start=raw_nav_table.date.min(),end=raw_nav_table.date.max(), freq="D")
+    everyday_table = pd.DataFrame(idx, columns=['date'])
+    everyday_nav_table = everyday_table.merge(raw_nav_table, how='left', on='date')
+    everyday_nav_table.set_index('date', inplace=True)
+    everyday_nav_table.interpolate(inplace=True)
+    return everyday_nav_table
+
+def load_gluonts_list_dataset(file_path):
+    """
+    Load a nav csv and convert it to gluonts ListDataset.
+
+    Args:
+        - file_path: the path of the nav csv file
+    Returns:
+        - data: the ListDataset
+    """
+    nav_table = load_raw_nav_table(file_path)
+    nav_table = fill_nav_dataframe(nav_table)
+    data = ListDataset([
+        {"start": nav_table.index[0],
+        "target": nav_table.value}],freq="D")
+    return data
+
+if __name__ == '__main__':
+    nav_files = os.listdir(NAV_DIR)
+    file_path = os.path.join(NAV_DIR, nav_files[0])
+    print(f"file_path: {file_path}")
+    dataset = load_gluonts_list_dataset(file_path)    
+    predictor = prophet.ProphetPredictor(
+        freq="D", prediction_length=1)
+    predictions = predictor.predict(dataset)    
+    print('Prediction Result:')
+    print(next(predictions))
