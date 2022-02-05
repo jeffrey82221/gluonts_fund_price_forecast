5fbd801f5a681601655b316d0479102050c5ff5c
jeffrey82221 <jeffrey82221@gmail.com>
2022-02-05 13:11:27 +0800

    [upgrade] cache

--- cachedir/joblib/fund_price_loader/load_nav_table/func_code.py
+++ cachedir/joblib/fund_price_loader/load_nav_table/func_code.py
@@ -1,15 +1,15 @@
-# first line: 23
+# first line: 28
 @memory.cache
 def load_nav_table(file_path):
     """
-    Load the NAV csv into pandas DataFrame and fill the missing 
+    Load the NAV csv into pandas DataFrame and fill the missing
     values.
 
     Args:
         - file_path: the path of the nav csv file
-    Returns: 
-        - nav_table: a pandas DataFrame of nav values without missing values 
-            of holiday. 
+    Returns:
+        - nav_table: a pandas DataFrame of nav values without missing values
+            of holiday.
     """
     nav_table = __load_raw_nav_table(file_path)
     nav_table = __fill_nav_dataframe(nav_table)
